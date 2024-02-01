962c12a0c24e8c09df875a77fd7ad5f08a2c22e5
FAZuH <fazuhhh@gmail.com>
2024-02-01 10:49:01 +0700

    renamed variables named id to id_ to prevent clashing with id()

--- vindicator/constants.py
+++ vindicator/constants.py
@@ -1,11 +1,8 @@
-from loguru import logger  # type: ignore
-
 from dotenv import dotenv_values
+from loguru import logger  # type: ignore
 
 
 config: dict[str, str] = dotenv_values(".env")  # type: ignore
 
-
-# Vindicator
 __version__ = "0.0.1"
 __author__ = "FAZuH"
