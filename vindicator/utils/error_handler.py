f3cdea0708ba15d9b9ec7cdb20ae47ecd247295f
FAZuH <fazuhhh@gmail.com>
2024-02-03 13:55:21 +0700

    removed lock_decorator(). improved retry decorator().

--- vindicator/utils/error_handler.py
+++ vindicator/utils/error_handler.py
@@ -1,7 +1,6 @@
-import asyncio
 from functools import wraps
 from inspect import iscoroutinefunction
-from typing import Any, Callable, Coroutine, ParamSpec, TypeVar, Iterable
+from typing import Awaitable, Callable, ParamSpec, TypeVar, Iterable
 
 from loguru import logger
 
@@ -11,51 +10,21 @@ P = ParamSpec("P")
 
 class ErrorHandler:
 
-    _locks: dict[str, asyncio.Lock] = {}
-
-    @classmethod
-    def lock_decorator(cls, lock_name: str):
-        """Locks the wrapped async function/method until the lock is released"""
-        def decorator(f: Callable[P, T]) -> Callable[P, Coroutine[Any, Any, T]]:
-            @wraps(f)
-            async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-                lock = cls._locks.get(lock_name)
-                if not lock:
-                    lock = asyncio.Lock()
-                    cls._locks[lock_name] = lock
-                async with lock:
-                    res = (await f(*args, **kwargs)) if iscoroutinefunction(f) else f(*args, **kwargs)
-                return res
-            return wrapper
-        return decorator
-
     @staticmethod
-    def retry_decorator(times: int, exceptions: type[BaseException] | Iterable[type[BaseException]]):
+    def retry_decorator(max_retries: int, exceptions: type[BaseException] | Iterable[type[BaseException]]):
         """ Retries the wrapped function/method `times` times if the exceptions listed in `exceptions` are thrown """
-        def decorator(f: Callable[P, T]) -> Callable[P, Coroutine[Any, Any, T]]:
+        def decorator(f: Callable[P, T]) -> Callable[P, Awaitable[T]]:
             @wraps(f)
-            async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-                attempt: int = 0
-                while True:
+            async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:  # type: ignore
+                for _ in range(max_retries + 1):
                     try:
-                        return (await f(*args, **kwargs)) if iscoroutinefunction(f) else f(*args, **kwargs)  # type: ignore
+                        return (await f(*args, **kwargs)) if iscoroutinefunction(f) else f(*args, **kwargs)
                     except exceptions:
-                        attempt += 1
-                        if attempt > times:
-                            logger.error(
-                                f"""
-                                {f.__qualname__} failed. Raising exception to main thread...\n
-                                args:{str(args)[:30]}\n
-                                kwargs:{str(kwargs)[:30]}
-                                """
-                            )
-                            raise
-                        logger.warning(
-                            f"""
-                            {f.__qualname__} failed. Retrying ({attempt}/{times})...\n
-                            args:{str(args)[:30]}\n
-                            kwargs:{str(kwargs)[:30]}\n
-                            """
-                        )
+                        logger.error((
+                            f"{f.__qualname__} failed. Raising exception to main thread...\n"
+                            f"args:{str(args)[:30]}\n"
+                            f"kwargs:{str(kwargs)[:30]}"
+                        ))
+                        raise
             return wrapper
         return decorator
