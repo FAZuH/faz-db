c11a766d0c0b45a87c0faf8b815d0c6bc7e80e3e
FAZuH <fazuhhh@gmail.com>
2024-02-03 14:31:08 +0700

    removed wynndata package. all files / packages within are pushed 1 level.

--- kans/__init__.py
+++ kans/__init__.py
@@ -13,7 +13,7 @@ from .api.wynn.model.field.uuid_field import UuidField
 from .app.abstract_fetch import AbstractFetch
 from .app.abstract_request import AbstractRequest
 from .app.request_level import RequestLevel
-from .db.wynndata.database import Database
+from .db.database import Database
 from .db.wynndata.model.date_column import DateColumn
 from .db.wynndata.model.gamemode_column import GamemodeColumn
 from .db.wynndata.model.uuid_column import UuidColumn
@@ -40,51 +40,51 @@ from .utils.http_request import HttpRequest  # ResponseSet
 from .api.wynn.wynn_api import WynnApi  # HttpRequest, Ratelimit, WynnResponse
 
 # db models.id
-from .db.wynndata.model.character_history.character_history_id import CharacterHistoryId
-from .db.wynndata.model.character_info.character_info_id import CharacterInfoId
-from .db.wynndata.model.guild_history.guild_history_id import GuildHistoryId
-from .db.wynndata.model.guild_info.guild_info_id import GuildInfoId
-from .db.wynndata.model.guild_member_history.guild_member_history_id import GuildMemberHistoryId
-from .db.wynndata.model.online_players.online_players_id import OnlinePlayersId
-from .db.wynndata.model.player_activity_history.player_activity_history_id import PlayerActivityHistoryId
-from .db.wynndata.model.player_history.player_history_id import PlayerHistoryId
-from .db.wynndata.model.player_info.player_info_id import PlayerInfoId
+from .db.model.character_history.character_history_id import CharacterHistoryId
+from .db.model.character_info.character_info_id import CharacterInfoId
+from .db.model.guild_history.guild_history_id import GuildHistoryId
+from .db.model.guild_info.guild_info_id import GuildInfoId
+from .db.model.guild_member_history.guild_member_history_id import GuildMemberHistoryId
+from .db.model.online_players.online_players_id import OnlinePlayersId
+from .db.model.player_activity_history.player_activity_history_id import PlayerActivityHistoryId
+from .db.model.player_history.player_history_id import PlayerHistoryId
+from .db.model.player_info.player_info_id import PlayerInfoId
 
 # db models. needs models.id
-from .db.wynndata.model.character_history.character_history import CharacterHistory
-from .db.wynndata.model.character_info.character_info import CharacterInfo
-from .db.wynndata.model.guild_info.guild_info import GuildInfo
-from .db.wynndata.model.guild_history.guild_history import GuildHistory
-from .db.wynndata.model.guild_member_history.guild_member_history import GuildMemberHistory
-from .db.wynndata.model.online_players.online_players import OnlinePlayers
-from .db.wynndata.model.player_activity_history.player_activity_history import PlayerActivityHistory
-from .db.wynndata.model.player_history.player_history import PlayerHistory
-from .db.wynndata.model.player_info.player_info import PlayerInfo
+from .db.model.character_history.character_history import CharacterHistory
+from .db.model.character_info.character_info import CharacterInfo
+from .db.model.guild_info.guild_info import GuildInfo
+from .db.model.guild_history.guild_history import GuildHistory
+from .db.model.guild_member_history.guild_member_history import GuildMemberHistory
+from .db.model.online_players.online_players import OnlinePlayers
+from .db.model.player_activity_history.player_activity_history import PlayerActivityHistory
+from .db.model.player_history.player_history import PlayerHistory
+from .db.model.player_info.player_info import PlayerInfo
 
 # db base. needs db models
-from .db.wynndata.repository.base.character_history_repo import CharacterHistoryRepo
-from .db.wynndata.repository.base.character_info_repo import CharacterInfoRepo
-from .db.wynndata.repository.base.guild_history_repo import GuildHistoryRepo
-from .db.wynndata.repository.base.guild_info_repo import GuildInfoRepo
-from .db.wynndata.repository.base.guild_member_history_repo import GuildMemberHistoryRepo
-from .db.wynndata.repository.base.online_players_repo import OnlinePlayersRepo
-from .db.wynndata.repository.base.player_activity_history_repo import PlayerActivityHistoryRepo
-from .db.wynndata.repository.base.player_history_repo import PlayerHistoryRepo
-from .db.wynndata.repository.base.player_info_repo import PlayerInfoRepo
+from .db.repository.base.character_history_repo import CharacterHistoryRepo
+from .db.repository.base.character_info_repo import CharacterInfoRepo
+from .db.repository.base.guild_history_repo import GuildHistoryRepo
+from .db.repository.base.guild_info_repo import GuildInfoRepo
+from .db.repository.base.guild_member_history_repo import GuildMemberHistoryRepo
+from .db.repository.base.online_players_repo import OnlinePlayersRepo
+from .db.repository.base.player_activity_history_repo import PlayerActivityHistoryRepo
+from .db.repository.base.player_history_repo import PlayerHistoryRepo
+from .db.repository.base.player_info_repo import PlayerInfoRepo
 
 # db tables. needs db base
-from .db.wynndata.repository.table.guild_history_table import GuildHistoryTable
-from .db.wynndata.repository.table.guild_info_table import GuildInfoTable
-from .db.wynndata.repository.table.guild_member_history_table import GuildMemberHistoryTable
-from .db.wynndata.repository.table.character_history_table import CharacterHistoryTable
-from .db.wynndata.repository.table.character_info_table import CharacterInfoTable
-from .db.wynndata.repository.table.online_players_table import OnlinePlayersTable
-from .db.wynndata.repository.table.player_activity_history_table import PlayerActivityHistoryTable
-from .db.wynndata.repository.table.player_history_table import PlayerHistoryTable
-from .db.wynndata.repository.table.player_info_table import PlayerInfoTable
+from .db.repository.table.guild_history_table import GuildHistoryTable
+from .db.repository.table.guild_info_table import GuildInfoTable
+from .db.repository.table.guild_member_history_table import GuildMemberHistoryTable
+from .db.repository.table.character_history_table import CharacterHistoryTable
+from .db.repository.table.character_info_table import CharacterInfoTable
+from .db.repository.table.online_players_table import OnlinePlayersTable
+from .db.repository.table.player_activity_history_table import PlayerActivityHistoryTable
+from .db.repository.table.player_history_table import PlayerHistoryTable
+from .db.repository.table.player_info_table import PlayerInfoTable
 
 # needs all tables above
-from .db.wynndata.wynndata_database import WynnDataDatabase
+from .db.wynndata_database import WynnDataDatabase
 
 # tasks. needs all above
 from .app.fetch_queue import FetchQueue  # Request
