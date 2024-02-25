1ad4b119b428f02f65ed79b58fa401649aa2e943
FAZuH <fazuhhh@gmail.com>
2024-02-25 17:19:11 +0700

    - missed refactor

--- kans/heartbeat/task/task_wynn_api_fetcher.py
+++ kans/heartbeat/task/task_wynn_api_fetcher.py
@@ -53,8 +53,8 @@ class WynnApiFetcher(Task):
             tasks_to_remove.append(req)
             if req.exception():
                 self._logger.error(f"Error fetching from Wynn API: {req.exception()}")
-                if req.get_coro().__qualname__ == self._api.players.get_uuid.__qualname__:
-                    self._request_list.put(0, self._api.players.get_uuid)
+                if req.get_coro().__qualname__ == self._api.player.get_online_uuids.__qualname__:
+                    self._request_list.put(0, self._api.player.get_online_uuids)
             else:
                 ok_results.append(req.result())
 
