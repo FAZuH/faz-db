2cdf797f4624729ae93f1a4c4ef3a41edfebecaf
FAZuH <fazuhhh@gmail.com>
2024-02-26 17:33:31 +0700

    misc

--- kans/api/wynn/response/abstract_wynn_response.py
+++ kans/api/wynn/response/abstract_wynn_response.py
@@ -4,10 +4,13 @@ from typing import TypeVar
 from ..model import Headers
 from kans.util import ResponseSet
 
-T = TypeVar("T")
+T = TypeVar('T')
 
 
 class AbstractWynnResponse(ResponseSet[T, Headers], ABC):
 
     def __repr__(self) -> str:
         return self.__class__.__name__
+    #
+    # def get_model_type(self) -> Type[T]:
+    #     return self.body.__class__
---
+++ tests/heartbeat/task/__init__.py
@@ -0,0 +1,2 @@
+# type: ignore
+from .test_response_list import TestResponseList
