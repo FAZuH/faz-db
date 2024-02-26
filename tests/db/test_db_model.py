63712670fe5a4fd0a1182fafd061687e7490372b
FAZuH <fazuhhh@gmail.com>
2024-02-26 10:24:34 +0700

    Fixed TestDbModel tests

--- tests/db/test_db_model.py
+++ tests/db/test_db_model.py
@@ -25,45 +25,47 @@ class TestDbModel(unittest.IsolatedAsyncioTestCase):
         self.mock_playerstats: list[PlayerResponse] = self.mockwynnapi.onlineplayerstats  # type: ignore
 
     async def test_character_history(self) -> None:
-        for datum in self.converter.to_character_history(self.mock_playerstats):
-            self.assertIsInstance(datum, CharacterHistory)
-            self.assertIsInstance(datum.character_uuid, UuidColumn)
-            self.assertIsInstance(datum.level, int)
-            self.assertIsInstance(datum.xp, int)
-            self.assertIsInstance(datum.wars, int)
-            self.assertIsInstance(datum.playtime, Decimal)
-            self.assertIsInstance(datum.mobs_killed, int)
-            self.assertIsInstance(datum.chests_found, int)
-            self.assertIsInstance(datum.logins, int)
-            self.assertIsInstance(datum.deaths, int)
-            self.assertIsInstance(datum.discoveries, int)
-            self.assertIsInstance(datum.gamemode, GamemodeColumn)
-            self.assertIsInstance(datum.alchemism, Decimal)
-            self.assertIsInstance(datum.armouring, Decimal)
-            self.assertIsInstance(datum.cooking, Decimal)
-            self.assertIsInstance(datum.jeweling, Decimal)
-            self.assertIsInstance(datum.scribing, Decimal)
-            self.assertIsInstance(datum.tailoring, Decimal)
-            self.assertIsInstance(datum.weaponsmithing, Decimal)
-            self.assertIsInstance(datum.woodworking, Decimal)
-            self.assertIsInstance(datum.mining, Decimal)
-            self.assertIsInstance(datum.woodcutting, Decimal)
-            self.assertIsInstance(datum.farming, Decimal)
-            self.assertIsInstance(datum.fishing, Decimal)
-            self.assertIsInstance(datum.dungeon_completions, int)
-            self.assertIsInstance(datum.quest_completions, int)
-            self.assertIsInstance(datum.raid_completions, int)
-            self.assertIsInstance(datum.datetime, DateColumn)
+        for stat in self.mock_playerstats:
+            for datum in self.converter.to_character_history(stat):
+                self.assertIsInstance(datum, CharacterHistory)
+                self.assertIsInstance(datum.character_uuid, UuidColumn)
+                self.assertIsInstance(datum.level, int)
+                self.assertIsInstance(datum.xp, int)
+                self.assertIsInstance(datum.wars, int)
+                self.assertIsInstance(datum.playtime, Decimal)
+                self.assertIsInstance(datum.mobs_killed, int)
+                self.assertIsInstance(datum.chests_found, int)
+                self.assertIsInstance(datum.logins, int)
+                self.assertIsInstance(datum.deaths, int)
+                self.assertIsInstance(datum.discoveries, int)
+                self.assertIsInstance(datum.gamemode, GamemodeColumn)
+                self.assertIsInstance(datum.alchemism, Decimal)
+                self.assertIsInstance(datum.armouring, Decimal)
+                self.assertIsInstance(datum.cooking, Decimal)
+                self.assertIsInstance(datum.jeweling, Decimal)
+                self.assertIsInstance(datum.scribing, Decimal)
+                self.assertIsInstance(datum.tailoring, Decimal)
+                self.assertIsInstance(datum.weaponsmithing, Decimal)
+                self.assertIsInstance(datum.woodworking, Decimal)
+                self.assertIsInstance(datum.mining, Decimal)
+                self.assertIsInstance(datum.woodcutting, Decimal)
+                self.assertIsInstance(datum.farming, Decimal)
+                self.assertIsInstance(datum.fishing, Decimal)
+                self.assertIsInstance(datum.dungeon_completions, int)
+                self.assertIsInstance(datum.quest_completions, int)
+                self.assertIsInstance(datum.raid_completions, int)
+                self.assertIsInstance(datum.datetime, DateColumn)
 
     async def test_character_info(self) -> None:
-        for datum in self.converter.to_character_info(self.mock_playerstats):
-            # assert instance of row members
-            self.assertIsInstance(datum.character_uuid, UuidColumn)
-            self.assertIsInstance(datum.uuid, UuidColumn)
-            self.assertIsInstance(datum.type, str)
+        for stat in self.mock_playerstats:
+            for datum in self.converter.to_character_info(stat):
+                self.assertIsInstance(datum.character_uuid, UuidColumn)
+                self.assertIsInstance(datum.uuid, UuidColumn)
+                self.assertIsInstance(datum.type, str)
 
     async def test_guild_history(self) -> None:
-        for datum in self.converter.to_guild_history(self.mock_guildstats):
+        for stat in self.mock_guildstats:
+            datum = self.converter.to_guild_history(stat)
             self.assertIsInstance(datum.name, str)
             self.assertIsInstance(datum.level, Decimal)
             self.assertIsInstance(datum.territories, int)
@@ -73,20 +75,22 @@ class TestDbModel(unittest.IsolatedAsyncioTestCase):
             self.assertIsInstance(datum.datetime, DateColumn)
 
     async def test_guild_info(self) -> None:
-        for datum in self.converter.to_guild_info(self.mock_guildstats):
+        for stat in self.mock_guildstats:
+            datum = self.converter.to_guild_info(stat)
             self.assertIsInstance(datum.name, str)
             self.assertIsInstance(datum.prefix, str)
             self.assertIsInstance(datum.created, DateColumn)
 
     async def test_guild_member_history(self) -> None:
-        for datum in self.converter.to_guild_member_history(self.mock_guildstats):
-            self.assertIsInstance(datum.uuid, UuidColumn)
-            self.assertIsInstance(datum.contributed, int)
-            self.assertIsInstance(datum.joined, DateColumn)
-            self.assertIsInstance(datum.datetime, DateColumn)
+        for stat in self.mock_guildstats:
+            for datum in self.converter.to_guild_member_history(stat):
+                self.assertIsInstance(datum.uuid, UuidColumn)
+                self.assertIsInstance(datum.contributed, int)
+                self.assertIsInstance(datum.joined, DateColumn)
+                self.assertIsInstance(datum.datetime, DateColumn)
 
     async def test_online_players(self) -> None:
-        for datum in self.converter.to_online_players((self.mock_onlineuuids,)):
+        for datum in self.converter.to_online_players(self.mock_onlineuuids):
             self.assertIsInstance(datum.uuid, UuidColumn)
             self.assertIsInstance(datum.server, str)
 
@@ -96,7 +100,8 @@ class TestDbModel(unittest.IsolatedAsyncioTestCase):
         pass
 
     async def test_player_history(self) -> None:
-        for datum in self.converter.to_player_history(self.mock_playerstats):
+        for stat in self.mock_playerstats:
+            datum = self.converter.to_player_history(stat)
             self.assertIsInstance(datum.uuid, UuidColumn)
             self.assertIsInstance(datum.username, str)
             self.assertIsInstance(datum.support_rank, (NoneType, str))
@@ -107,7 +112,8 @@ class TestDbModel(unittest.IsolatedAsyncioTestCase):
             self.assertIsInstance(datum.datetime, DateColumn)
 
     async def test_player_info(self) -> None:
-        for datum in self.converter.to_player_info(self.mock_playerstats):
+        for stat in self.mock_playerstats:
+            datum = self.converter.to_player_info(stat)
             self.assertIsInstance(datum.uuid, UuidColumn)
             self.assertIsInstance(datum.latest_username, str)
             self.assertIsInstance(datum.first_join, DateColumn)
