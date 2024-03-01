dfdc5815c0077e7a696b1179ae760231bf97a91e
FAZuH <fazuhhh@gmail.com>
2024-03-02 03:57:34 +0700

    Add more info on TaskStatusReport report message

--- kans/heartbeat/task/task_status_report.py
+++ kans/heartbeat/task/task_status_report.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 import platform
 import psutil
 from typing import TYPE_CHECKING
@@ -11,7 +11,6 @@ import discord
 from . import Task
 
 if TYPE_CHECKING:
-    from datetime import timedelta
     from loguru import Logger
     from . import RequestList, TaskApiRequest, TaskDbInsert
     from kans import Api, ConfigT, Database
@@ -82,36 +81,47 @@ class TaskStatusReport(Task):
 
                 await message.edit(content=report)
 
-    _DEFAULT_MSG = \
-            "```yaml"\
-            "\n."\
-            "\n├── Kans Stats/"\
-            "\n│   ├── Runtime : {}"\
-            "\n│   └── RAM     : {}"\
-            "\n├── Tasks/"\
-            "\n│   ├── ApiRequest Latest   : {}"\
-            "\n│   ├── DbInsert Latest     : {}"\
-            "\n│   ├── StatusReport Latest : {}"\
-            "\n│   └── RequestList/"\
-            "\n│       └── [Queued | Eligible | Running]/"\
-            "\n│           ├── PlayerStat    : [ {:<4} | {:<4} | {:<4} ]"\
-            "\n│           ├── OnlinePlayers : [ {:<4} | {:<4} | {:<4} ]"\
-            "\n│           └── GuildStat     : [ {:<4} | {:<4} | {:<4} ]"\
-            "\n├── Db Stats/"\
-            "\n│   └── Size : {}"\
-            "\n├── Api Stats/"\
-            "\n│   ├── Ratelimit     : {}"\
-            "\n│   ├── Online Player : {}"\
-            "\n│   └── Online Guild  : {}"\
-            "\n└── System Stats/"\
-            "\n    ├── OS     : {}"\
-            "\n    └── Uptime : {}"\
+    _DEFAULT_MSG = (
+            "```yaml"
+            "\n."
+            "\n├── Kans Stats/"
+            "\n│   ├── Runtime : {}"
+            "\n│   └── RAM     : {:.2f} MB"
+            "\n│"
+            "\n├── Tasks/"
+            "\n│   ├── ApiRequest Latest   : {}"
+            "\n│   ├── DbInsert Latest     : {}"
+            "\n│   ├── StatusReport Latest : {}"
+            "\n│   └── RequestList/"
+            "\n│       └── [Queued | Eligible | Running]/"
+            "\n│           ├── GuildStat     : [ {:<4} | {:<4} | {:<4} ]"
+            "\n│           ├── OnlinePlayers : [ {:<4} | {:<4} | {:<4} ]"
+            "\n│           └── PlayerStat    : [ {:<4} | {:<4} | {:<4} ]"
+            "\n│"
+            "\n├── Db Stats/"
+            "\n│   └── Size : {:.2f} MB"
+            "\n├"
+            "\n├── Api Stats/"
+            "\n│   ├── Ratelimit     : {}"
+            "\n│   ├── Online Player : {}"
+            "\n│   ├── Online Guild  : {}"
+            "\n│   ├── Avg Guild Req Duration         : {:.2f}"
+            "\n│   ├── Avg OnlinePlayers Req Duration : {:.2f}"
+            "\n│   ├── Avg Player Req Duration        : {:.2f}"
+            "\n│   ├── Guild Reqs Within 5m         : {}"
+            "\n│   ├── OnlinePlayers Reqs Within 5m : {}"
+            "\n│   ├── Player Reqs Within 5m        : {}"
+            "\n│   └── All Reqs Within 5m           : {}"
+            "\n│"
+            "\n└── System Stats/"
+            "\n    ├── OS     : {}"
+            "\n    └── Uptime : {}"
             "\n```"
+    )
 
-    # TODO: unique count of request types on RequestList
-    # TODO: average requests per minute
     # TODO: clean code
     async def _get_report(self) -> str:
+        db_total_size = asyncio.create_task(self._db.total_size())
         now = datetime.now()
         now_ts = now.timestamp()
 
@@ -138,31 +148,53 @@ class TaskStatusReport(Task):
             unique_request_list["running"][index] += 1
 
         DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+        # TODO: hacky way to do these. change later
+        avg_guild_req_duration = self._api.performance_recorder.get_average("guild.get")
+        avg_onlineplayers_req_duration = self._api.performance_recorder.get_average("player.get_online_uuids")
+        avg_player_req_duration = self._api.performance_recorder.get_average("player.get_full_stats")
+        avg_req_duration = (avg_guild_req_duration + avg_onlineplayers_req_duration + avg_player_req_duration) / 3
+
+        recent_guild_reqs = len(self._api.performance_recorder.get_recent(timedelta(minutes=5), "guild.get"))
+        recent_onlineplayers_reqs = len(self._api.performance_recorder.get_recent(timedelta(minutes=5), "player.get_online_uuids"))
+        recent_player_reqs = len(self._api.performance_recorder.get_recent(timedelta(minutes=5), "player.get_full_stats"))
+        recent_all_reqs = recent_guild_reqs + recent_onlineplayers_reqs + recent_player_reqs
+
         msg = self._DEFAULT_MSG.format(
                 now - self._start_time,  # runtime
-                f"{self.Util.get_memory_usage() / self.Util.MB_TO_BYTE} MB",  # ram
+                self.Util.get_memory_usage() / self.Util.MB_TO_BYTE,  # ram
 
                 self._api_request.latest_run.strftime(DATETIME_FORMAT),  # api request latest
                 self._db_insert.latest_run.strftime(DATETIME_FORMAT),  # db insert latest
                 self._latest_run.strftime(DATETIME_FORMAT),  # status report latest
 
-                unique_request_list["queued"][0],  # queued player stat
-                unique_request_list["eligible"][0],  # eligible player stat
-                unique_request_list["running"][0],  # running player stat
+                unique_request_list["queued"][2],  # queued guild stat
+                unique_request_list["eligible"][2],  # eligible guild stat
+                unique_request_list["running"][2],  # running guild stat
 
                 unique_request_list["queued"][1],  # queued online players
                 unique_request_list["eligible"][1],  # eligible online players
                 unique_request_list["running"][1],  # running online players
 
-                unique_request_list["queued"][2],  # queued guild stat
-                unique_request_list["eligible"][2],  # eligible guild stat
-                unique_request_list["running"][2],  # running guild stat
+                unique_request_list["queued"][0],  # queued player stat
+                unique_request_list["eligible"][0],  # eligible player stat
+                unique_request_list["running"][0],  # running player stat
 
-                f"{await self._db.total_size() / self.Util.MB_TO_BYTE} MB",  # db size
+                (await db_total_size) / self.Util.MB_TO_BYTE,  # db size
 
                 self._api.ratelimit.remaining,  # ratelimit
                 len(self._db_insert.response_handler.online_players),  # online player
                 len(self._db_insert.response_handler.online_guilds),  # online guild
+
+                avg_guild_req_duration,  # avg guild req duration
+                avg_onlineplayers_req_duration,  # avg onlineplayers req duration
+                avg_player_req_duration,  # avg player req duration
+                avg_req_duration,  # avg req duration
+
+                recent_guild_reqs,  # n amount of recent guild reqs
+                recent_onlineplayers_reqs,  # n amount of recent onlineplayers reqs
+                recent_player_reqs,  # n amount of recent player reqs,
+                recent_all_reqs,  # n amount of recent all reqs
 
                 self.Util.get_os_info(),  # os
                 self.Util.get_os_uptime()  # uptime
