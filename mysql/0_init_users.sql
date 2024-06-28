9ace206b47241f215c2c6c48ba2f5f96ca40f033
FAZuH <fazuhhh@gmail.com>
2024-06-28 14:54:02 +0700

    chore: remove build files. faz-bot will be deployed on faz-docker

--- mysql/0_init_users.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `wynndb`;
-CREATE DATABASE IF NOT EXISTS `wynndb_test`;
-
-CREATE USER IF NOT EXISTS 'fazbot'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `wynndb`.* TO 'fazbot'@'localhost';
-GRANT ALL PRIVILEGES ON `wynndb_test`.* TO 'fazbot'@'localhost';
-FLUSH PRIVILEGES;
--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call .venv/Scripts/activate
-python -m wynndb
-call .venv/Scripts/deactivate
-
-pause
