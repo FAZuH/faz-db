5a8511c665945c71bf7ae43d5091d8c4d11b467e
FAZuH <fazuhhh@gmail.com>
2024-06-29 03:34:21 +0700

    fix: remove input() on __main__ to prevent EOFError on docker

--- fazdb/__main__.py
+++ fazdb/__main__.py
@@ -1,37 +1,26 @@
 from __future__ import annotations
 import asyncio
 from time import sleep
-from typing import TYPE_CHECKING
 
 from fazdb.app import FazDb
-
-if TYPE_CHECKING:
-    from fazdb import App
 
 
 class Main:
 
-    def __init__(self) -> None:
-        self._app = FazDb()
+    app = FazDb()
 
-    def main(self) -> None:
-        self._app.start()
+    @classmethod
+    def main(cls) -> None:
+        cls.app.start()
+        while True:  # keep-alive
+            sleep(69_420)
 
-        while True:
-            inp = input()
-            if inp == "exit":
-                self._app.stop()
-                exit(0)
-            sleep(0.1)
-
-    @property
-    def app(self) -> App:
-        return self._app
 
 if __name__ == "__main__":
-    main = Main()
     try:
-        main.main()
+        Main.main()
     except Exception as e:
-        asyncio.run(main.app.logger.discord.error("FATAL ERROR", e))
+        logger = Main.app.logger
+        logger.console.exception(str(e))
+        asyncio.run(logger.discord.error(f"FATAL ERROR", e))
         exit(1)
