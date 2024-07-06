dae311a28685fe94f9a46f3d7dd08a368c1b272d
FAZuH <fazuhhh@gmail.com>
2024-07-06 11:37:36 +0700

    chore(db): Remove OnlinePlayersRepository.insert_new_online_players()

--- fazdb/db/fazdb/repository/online_players_repository.py
+++ fazdb/db/fazdb/repository/online_players_repository.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
-from typing import Any, TYPE_CHECKING, Iterable
+from typing import Any, TYPE_CHECKING
 
 from ..model import OnlinePlayers
 from ._repository import Repository
 
 if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
     from ... import BaseAsyncDatabase
 
 
@@ -13,8 +12,3 @@ class OnlinePlayersRepository(Repository[OnlinePlayers, Any]):
 
     def __init__(self, database: BaseAsyncDatabase[Any]) -> None:
         super().__init__(database, OnlinePlayers)
-
-    async def insert_new_online_players(self, entities: Iterable[OnlinePlayers], session: AsyncSession | None = None) -> None:
-        async with self.database.must_enter_session(session) as session:
-            await self.truncate(session)
-            await self.insert(entities, session, replace_on_duplicate=True)
