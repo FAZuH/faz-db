7b1155f454cf09547bd26cd68a28b4566fb37df1
FAZuH <fazuhhh@gmail.com>
2024-07-09 16:25:12 +0700

    refactor(db)!: Change primary key from name to uuid in guild_info table
    BREAKING CHANGE: Structure for table guild_info is modified on source
    code. Modify existing database with the following SQL query:
    
    1. ALTER TABLE guild_info DROP PRIMARY KEY;
    
    2. ALTER TABLE guild_info ADD PRIMARY KEY (uuid);

--- fazdb/db/fazdb/model/guild_info.py
+++ fazdb/db/fazdb/model/guild_info.py
@@ -9,7 +9,7 @@ from . import BaseModel
 class GuildInfo(BaseModel):
     __tablename__ = "guild_info"
 
-    name: Mapped[str] = mapped_column(VARCHAR(30), primary_key=True, nullable=False)
+    uuid: Mapped[bytes] = mapped_column(BINARY(16), primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(VARCHAR(30), nullable=False)
     prefix: Mapped[str] = mapped_column(VARCHAR(4), nullable=False)
     created: Mapped[dt] = mapped_column(DATETIME, nullable=False)
-    uuid: Mapped[bytes] = mapped_column(BINARY(16), nullable=False)
